9a36bda3275a27eee06ce47b2698f88122bc5f16
Nikita Marshchonok <n.marshchonok@gmail.com>
2026-05-23 23:07:32 +0300

    spark.read

--- spark/jobs/feature_engineering.py
+++ spark/jobs/feature_engineering.py
@@ -7,3 +7,24 @@ spark = SparkSession.builder.appName('RecSys Feature Engineering').master('local
 
 spark.sparkContext.setLogLevel('ERROR')
 print('Spark Session created succsessfully')
+
+
+# Читаем сырые данные
+ratings = spark.read \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .csv("data/raw/ratings.csv")
+
+movies = spark.read \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .csv("data/raw/movies.csv")
+
+users = spark.read \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .csv("data/raw/users.csv")
+
+print(f"Оценок: {ratings.count()}")
+print(f"Фильмов: {movies.count()}")
+print(f"Пользователей: {users.count()}")
